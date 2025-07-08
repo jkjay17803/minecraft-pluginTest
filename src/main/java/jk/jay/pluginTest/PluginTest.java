9c14fccbca3b28a7103e89783336f1c5368bd0c0
JK <jkjay17803@gmail.com>
2025-07-08 22:56:16 +0900

    jk

---
+++ src/main/java/jk/jay/pluginTest/PluginTest.java
@@ -0,0 +1,17 @@
+package jk.jay.pluginTest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLo
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
